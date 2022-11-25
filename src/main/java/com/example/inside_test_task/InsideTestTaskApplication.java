5f2af1ed6f0079c44aab3ca26271953f5229ec23
architect3232 <mr.moron56@gmail.com>
2022-11-25 15:58:11 +0500

    created project

---
+++ src/main/java/com/example/inside_test_task/InsideTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.inside_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsideTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsideTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inside_test_task/InsideTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inside_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsideTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
